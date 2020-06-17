cba4dba490a77d7a60e52269cc2ef9964ed57f92
RicardoBaniski <ricardo.baniski@opet.edu.br>
2020-06-16 23:07:14 -0300

    Tratamento da Informação
    Lógica de tratamento da informação extraída do banco de dados da aplicação

---
+++ DataScience/Condominvs/Condominio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Condominvs
+{
+    public class Condominio
+    {
+        private string first_name;
+        private string last_name;
+        private string photo;
+        private int block;
+        private int apt;
+        private string car;
+        private string garage;
+        private string pet;
+        private string gender;
+        private string phone;
+        private float salary;
+        private string job;
+        private DateTime birthday;
+        private string older;
+        private int age;
+        private string email;
+        private string obs;
+
+        public string First_name { get => first_name; set => first_name = value; }
+        public string Last_name { get => last_name; set => last_name = value; }
+        public string Photo { get => photo; set => photo = value; }
+        public int Block { get => block; set => block = value; }
+        public int Apt { get => apt; set => apt = value; }
+        public string Car { get => car; set => car = value; }
+        public string Garage { get => garage; set => garage = value; }
+        public string Pet { get => pet; set => pet = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public float Salary { get => salary; set => salary = value; }
+        public string Job { get => job; set => job = value; }
+        public DateTime Birthday { get => birthday; set => birthday = value; }
+        public string Older { get => older; set => older = value; }
+        public int Age { get => age; set => age = value; }
+        public string Email { get => email; set => email = value; }
+        public string Obs { get => obs; set => obs = value; }
+    }
+}
---
+++ DataScience/Condominvs/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using System.Globalization;
+using System.Configuration;
+
+namespace Condominvs
+{
+    class Program
+    {
+        public static string path = @"C:\Users\rbani\Documents\OPET\Condominvs_PI\DataScience";
+        public static SqlConnection conn = new SqlConnection(@"Data Source=AVELL\SQLEXPRESS;Initial Catalog=Condominvs;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public static SqlCommand cmd = new SqlCommand();
+        public static Condominio cond = new Condominio();
+        public static string[] header;
+        public static string[] formattedColumn;
+        public static string archive;
+        public static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(path, "*.csv");
+            foreach (var file in files)
+            {
+                int count = 1;
+                archive = file;
+                if (File.Exists(file))
+                {
+                    var reader = new StreamReader(file, Encoding.UTF8, true);
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            string[] separationColumns = line.Split(',');
+                            //if (count == 1)
+                            //{
+                            //    header = separationColumns;
+                            //}                           
+                        }
+                        count++;
+                        Console.WriteLine("Line: " + count + " - File: " + file);
+                    }
+                }
+            }
+            Console.WriteLine(">>>>>>>>>>>>>  SUCCESSFULLY COMPLETED  <<<<<<<<<<<<<");
+            Console.ReadKey();
+        }
+
+        public static void InsertObj(string[] separationColumns)
+        {
+            cond.First_name = separationColumns[1];
+            cond.Last_name = separationColumns[2];
+            cond.Photo = separationColumns[3];
+            cond.Block = separationColumns[4];
+            cond.Apt = separationColumns[5];
+            cond.Car = separationColumns[6];
+            cond.Garage = separationColumns[7];
+            cond.Pet = separationColumns[8];
+            cond.Gender = separationColumns[9];
+            cond.Phone = separationColumns[10];
+            cond.Salary = separationColumns[11];
+            cond.Job = separationColumns[12];
+            cond.Birthday = separationColumns[13];
+            cond.Older = separationColumns[14];
+            cond.Age = separationColumns[15];
+            cond.Email = separationColumns[16];
+            cond.Obs = separationColumns[17];
+            InsertSQL(conn, ref cmd, cond);
+        }
+
+        private static void InsertSQL(SqlConnection conn, ref SqlCommand cmd, Condominio cond)
+        {
+            
+        }
+    }
+}
